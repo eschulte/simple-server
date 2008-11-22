9a23af0228c6b11fd8855eef2bbb2731326fa594
Eric Schulte <schulte.eric@gmail.com>
2008-11-21 18:59:43 -0800

    growing

--- simple-server.el
+++ simple-server.el
@@ -21,7 +21,7 @@
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-;;; Attribution:
+;;; Attribution (read: unwitting authors) :
 ;;
 ;; This borrows heavily from the following
 ;;
